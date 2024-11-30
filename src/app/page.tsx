c32dd2603d921f9c41f09595f2cf72c6046aaff2
Rojen Rosal <rojenrosal@gmail.com>
2024-11-30 15:05:13 +0800

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ export default function HomePage() {
     <div>
       <h1 className="text-2xl font-bold">Welcome to PnF Badminton Club!</h1>
       <p className="mt-2">
-        Track teams, matches, and results. Register your team and start playing!
+Paps Badminton Christmas league Special
       </p>
     </div>
   );
